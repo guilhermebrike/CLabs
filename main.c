b15c6f361fbc5666534edb4437d59bfc0695445e
Guilherme <guilhermebrike@gmail.com>
2019-02-11 12:58:30 -0800

    finished

--- main.c
+++ main.c
@@ -2,56 +2,42 @@
 #include <string.h>
 
 
+// transform numbers in hexadecimal
+                // while(n!=0) {    
+                //     int mod_number  = 0; 
+                //     mod_number = n % 16; 
+                //     if(mod_number < 10) { 
+                //         mod_number = mod_number + 48; 
+                //         putchar(mod_number);
+                //     }else{ 
+                //         mod_number = mod_number + 87; 
+                //         putchar(mod_number);
+                //         } 
+                //     n = n/16; 
+                // }
 
-int vc_strlen(char *str) {
 
-    int i = 0;
-    while (str[i] != '\0') {
-        i++;
-    }
-    return i;
-}
-
-unsigned int *vc_strlcpy(char *dest, char *src, unsigned int size){
-    int dest_size = vc_strlen(dest);
-    int src_size = vc_strlen(src);
-
-    int i = 0;
-    while (dest[i] != '\0'){
-        dest[i] = '\0';
-        i++;
-    }
-    for(i = 0; i < size -1; i++){
-        dest[i] = src[i];
-    }
-    return src_size;
-}
-
+ void *vc_print_memory(void *addr, unsigned int size){
+     putchar(addr++);
+     putchar(addr++);
+     putchar(addr++);
+     putchar(addr++);
+     putchar(addr++);
+     putchar(addr++);
+ }
 
 int main() {
+
+
     // when creating a char array using the [] notation it will convert to a normal char * with \0 at the end
     
-    //dst
-    char str1[] = "1234\0         ";
-    //src
-    char str2[] = "hello";
+    char *test = "Salut les aninches c'est cool show non on fait de tructerrible\x00\x2e\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0e\x0f\x1b\x7f";
 
-    //dst
-    char str3[] = "1234\0         ";
-    //src
-    char str4[] = "hello";
+    vc_print_memory(test,10);
+
 
     // the size n parameter is that notting will be writen after dest+size-1
-    printf("%s\n","UNIX");
-    printf("%d",strlcpy(str1,str2,10));
-    putchar('\n');
-    printf("%s",str1);
-    putchar('\n');
-    printf("%s\n","MINE");
-    printf("%d",vc_strlcpy(str3,str4,10));
-    putchar('\n');
-    printf("%s",str3);
-    putchar('\n');
+
 
     // printf("%d\n", vc_strlen(str2));
     // printf("%d\n", vc_strlen(str3));
@@ -69,3 +55,11 @@ int main() {
     //vc_print_memory(str2,3);
     return 0;
 }
+
+// int main(void) {
+
+//     int a;
+//     a = 5;
+//     printf("The memory address of a is: %p\n", (void*) &a);
+//     return 0;
+// }
